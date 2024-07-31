e757b42257ce1c89c938343905f286cc9f516f23
Ivan85AC <Chernyhia@rambler.ru>
2024-07-31 17:53:43 +0500

    Add files via upload

---
+++ Module_4_1.py
@@ -0,0 +1,12 @@
+import Fake_math as fm
+import True_math as tm
+
+result1 = fm.divide(10, 2)
+result2 = fm.divide(10, 0)
+result3 = tm.divide(24, 4)
+result4 = tm.divide(24, 0)
+
+print(result1)
+print(result2)
+print(result3)
+print(result4)
